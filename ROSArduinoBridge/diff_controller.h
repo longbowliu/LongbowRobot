dcb96128bebd422e6c823412a4b289aee1d464b3
longbowliu <llb_feng@163.com>
2017-06-18 16:17:02 +0800

    Add files via upload
    --

---
+++ ROSArduinoBridge/diff_controller.h
@@ -0,0 +1,224 @@
+
+
+/* PID setpoint info For a Motor */
+typedef struct {
+  double TargetTicksPerFrame;    // target speed in ticks per frame
+  long Encoder;                  // encoder count
+  long PrevEnc;                  // last encoder count
+
+  /*
+  * Using previous input (PrevInput) instead of PrevError to avoid derivative kick,
+  * see http://brettbeauregard.com/blog/2011/04/improving-the-beginner%E2%80%99s-pid-derivative-kick/
+  */
+  int PrevInput;                // last input
+  //int PrevErr;                   // last error
+
+  /*
+  * Using integrated term (ITerm) instead of integrated error (Ierror),
+  * to allow tuning changes,
+  * see http://brettbeauregard.com/blog/2011/04/improving-the-beginner%E2%80%99s-pid-tuning-changes/
+  */
+  //int Ierror;
+  int ITerm;                    //integrated term
+
+  long output;                    // last motor setting
+}
+SetPointInfo;
+
+SetPointInfo leftPID;
+SetPointInfo rightPID;
+
+/* PID Parameters */
+int Kp = 20;
+int Kd = 12;
+int Ki = 0;
+int Ko = 50;
+
+int left_Kp=Kp;
+int left_Kd=Kd;
+int left_Ki=Ki;
+int left_Ko=Ko;
+
+int right_Kp=Kp;
+int right_Kd=Kd;
+int right_Ki=Ki;
+int right_Ko=Ko;
+
+unsigned char moving = 0; // is the base in motion?
+
+/*
+* Initialize PID variables to zero to prevent startup spikes
+* when turning PID on to start moving
+* In particular, assign both Encoder and PrevEnc the current encoder value
+* See http://brettbeauregard.com/blog/2011/04/improving-the-beginner%E2%80%99s-pid-initialization/
+* Note that the assumption here is that PID is only turned on
+* when going from stop to moving, that's why we can init everything on zero.
+*/
+void resetPID(){
+   leftPID.TargetTicksPerFrame = 0.0;
+   leftPID.Encoder = readEncoder(LEFT);
+   leftPID.PrevEnc = leftPID.Encoder;
+   leftPID.output = 0;
+   leftPID.PrevInput = 0;
+   leftPID.ITerm = 0;
+
+   rightPID.TargetTicksPerFrame = 0.0;
+   rightPID.Encoder = readEncoder(RIGHT);
+   rightPID.PrevEnc = rightPID.Encoder;
+   rightPID.output = 0;
+   rightPID.PrevInput = 0;
+   rightPID.ITerm = 0;
+}
+
+/* PID routine to compute the next motor commands */
+void dorightID(SetPointInfo * p) {
+  long Perror;
+  long output;
+  int input;
+
+  //Perror = p->TargetTicksPerFrame - (p->Encoder - p->PrevEnc);
+  input = p->Encoder - p->PrevEnc;
+  Perror = p->TargetTicksPerFrame - input;
+
+
+  /*
+  * Avoid derivative kick and allow tuning changes,
+  * see http://brettbeauregard.com/blog/2011/04/improving-the-beginner%E2%80%99s-pid-derivative-kick/
+  * see http://brettbeauregard.com/blog/2011/04/improving-the-beginner%E2%80%99s-pid-tuning-changes/
+  */
+  //output = (Kp * Perror + Kd * (Perror - p->PrevErr) + Ki * p->Ierror) / Ko;
+  // p->PrevErr = Perror;
+  output = (right_Kp * Perror - right_Kd * (input - p->PrevInput) + p->ITerm) / right_Ko;
+
+  p->PrevEnc = p->Encoder;
+
+  output += p->output;
+  // Accumulate Integral error *or* Limit output.
+  // Stop accumulating when output saturates
+
+  if (output >= MAX_PWM)
+    output = MAX_PWM;
+  else if (output <= -MAX_PWM)
+    output = -MAX_PWM;
+  else
+  /*
+  * allow turning changes, see http://brettbeauregard.com/blog/2011/04/improving-the-beginner%E2%80%99s-pid-tuning-changes/
+  */
+    p->ITerm += right_Ki * Perror;
+  
+
+
+  p->output = output;
+
+  p->PrevInput = input;
+}
+
+/* PID routine to compute the next motor commands */
+void doleftPID(SetPointInfo * p) {
+  long Perror;
+  long output;
+  int input;
+  
+  //Perror = p->TargetTicksPerFrame - (p->Encoder - p->PrevEnc);
+  input = p->Encoder - p->PrevEnc;
+  Perror =p->TargetTicksPerFrame - input;
+
+  
+  
+
+  /*
+  * Avoid derivative kick and allow tuning changes,
+  * see http://brettbeauregard.com/blog/2011/04/improving-the-beginner%E2%80%99s-pid-derivative-kick/
+  * see http://brettbeauregard.com/blog/2011/04/improving-the-beginner%E2%80%99s-pid-tuning-changes/
+  */
+  //output = (Kp * Perror + Kd * (Perror - p->PrevErr) + Ki * p->Ierror) / Ko;
+  // p->PrevErr = Perror;
+  output = (left_Kp * Perror - left_Kd * (input - p->PrevInput) + p->ITerm) / left_Ko;
+
+  p->PrevEnc = p->Encoder;
+
+  output += p->output;
+  // Accumulate Integral error *or* Limit output.
+  // Stop accumulating when output saturates
+  /*
+  if(output>=0){
+    if(output <80){
+      output = 80;
+    }
+    else if (output >= MAX_PWM){
+      output = MAX_PWM;
+    }else{
+       p->ITerm += left_Ki * Perror;
+    }
+  }
+  else{
+    if(output>-80){
+      output = -80;
+    }
+    else if (output <= -MAX_PWM){
+      output = -MAX_PWM;
+    }else{
+       p->ITerm += left_Ki * Perror;
+    }
+  }
+  */
+  
+    if (output >= MAX_PWM)
+    output = MAX_PWM;
+  else if (output <= -MAX_PWM)
+    output = -MAX_PWM;
+  else
+  /*
+  * allow turning changes, see http://brettbeauregard.com/blog/2011/04/improving-the-beginner%E2%80%99s-pid-tuning-changes/
+  */
+    p->ITerm += left_Ki * Perror;
+  
+ 
+  /*
+  * allow turning changes, see http://brettbeauregard.com/blog/2011/04/improving-the-beginner%E2%80%99s-pid-tuning-changes/
+  */
+   
+
+  p->output = output;
+  p->PrevInput = input;
+}
+
+/* Read the encoder values and call the PID routine */
+void updatePID() {
+  /* Read the encoders */
+  leftPID.Encoder = readEncoder(LEFT);
+
+  
+  rightPID.Encoder = readEncoder(RIGHT);
+  
+
+  
+  /* If we're not moving there is nothing more to do */
+  if (!moving){
+    /*
+    * Reset PIDs once, to prevent startup spikes,
+    * see http://brettbeauregard.com/blog/2011/04/improving-the-beginner%E2%80%99s-pid-initialization/
+    * PrevInput is considered a good proxy to detect
+    * whether reset has already happened
+    */
+    if (leftPID.PrevInput != 0 || rightPID.PrevInput != 0) resetPID();
+    return;
+  }
+
+  doleftPID(&leftPID);//执行左马达PID
+  dorightID(&rightPID);//执行右马达PID
+ 
+
+  /* Set the motor speeds accordingly */
+  int adjustment = 0;
+  int diff = rightPID.Encoder - leftPID.Encoder;
+  if(leftPID.output>0){
+    adjustment = 30;
+    adjustment += diff*10;
+  }else{
+    adjustment = -30;
+    adjustment += -diff*10;
+  }
+  setMotorSpeeds(leftPID.output+adjustment, rightPID.output);
+}
+
