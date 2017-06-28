3bb474234c7340bc28037812aaa4c8ab8ae9d883
longbowliu <llb_feng@163.com>
2017-06-28 22:59:40 +0800

    remove degree and raidians

--- ros_arduino_bridge/ros_arduino_python/src/ros_arduino_python/arduino_driver.py
+++ ros_arduino_bridge/ros_arduino_python/src/ros_arduino_python/arduino_driver.py
@@ -334,13 +334,13 @@ class Arduino:
         ''' Usage: servo_write(id, pos)
             Position is given in radians and converted to degrees before sending
         '''        
-        return self.execute_ack('s %d %d' %(id, min(SERVO_MAX, max(SERVO_MIN, degrees(pos)))))
+        return self.execute_ack('s %d %d' %(id, min(SERVO_MAX, max(SERVO_MIN, pos))))
     
     def servo_read(self, id):
         ''' Usage: servo_read(id)
             The returned position is converted from degrees to radians
         '''        
-        return radians(self.execute('t %d' %id))
+        return self.execute('t %d' %id)
 
     def ping(self, pin):
         ''' The srf05/Ping command queries an SRF05/Ping sonar sensor
