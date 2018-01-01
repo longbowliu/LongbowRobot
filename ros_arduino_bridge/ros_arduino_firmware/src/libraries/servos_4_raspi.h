4665dbe1a90096ef4b0f86869470cb3693eb501f
longbowliu <llb_feng@163.com>
2018-01-01 12:20:47 +0800

    nothing

--- ros_arduino_bridge/ros_arduino_firmware/src/libraries/servos_4_raspi.h
+++ ros_arduino_bridge/ros_arduino_firmware/src/libraries/servos_4_raspi.h
@@ -2,7 +2,7 @@
 #define SERVOS_H
 
 
-#define N_SERVOS 6
+#define N_SERVOS 9
 
 // This delay in milliseconds determines the pause 
 // between each one degree step the servo travels.  Increasing 
@@ -10,12 +10,11 @@
 // Decreasing this number will make the servo sweep more quickly.
 // Zero is the default number and will make the servos spin at
 // full speed.  150 ms makes them spin very slowly.
-int stepDelay [N_SERVOS] = { 15,15,15,15,15,15 }; // ms
+int stepDelay [N_SERVOS] = { 15,15,15,15,15,15 ,15,15,15}; // ms
 
 // Pins
 
-//byte servoPins [N_SERVOS] = {11,13,12,15,8,14,50,52,53};
-byte servoPins [N_SERVOS] = {2,3,4,5,6,7};
+byte servoPins [N_SERVOS] = {11,13,12,15,8,14,50,52,53};
 
 
 // !!!!!!!!!!!!!!!!!!!frond --> behind && left--> right!!!!!!!!!!!!!!!!!!!!
@@ -32,7 +31,7 @@ byte servoPins [N_SERVOS] = {2,3,4,5,6,7};
 
 
 // Initial Position
-byte servoInitPosition [N_SERVOS] = {60,60,60,60,60,60}; // [0, 180] degrees
+byte servoInitPosition [N_SERVOS] = {60,130,100,5,0,1,80,90,40 }; // [0, 180] degrees
 
 
 class SweepServo
